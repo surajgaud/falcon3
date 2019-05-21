f058a59b97d6789592d1bd7850a5d30e460947fc
Suraj Gaud <suraj.gaud@autodesk.com>
2019-05-21 13:36:59 -0700

    jsconfig update

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_sJVDa752B', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '13p317lv3aig9huuvq48kgpqi4', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_9PW9bea1L', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '38orncaaclfq6ahmo8soe2k1jc', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
